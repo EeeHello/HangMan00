fbc2cc4f6cb9311b7bf166b9d172993824d911ea
EeeHello <thitiwich33@gmail.com>
2023-09-04 09:25:32 +0200

    Add project files.

---
+++ HangMan00/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace HangMan00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Welcome to Hangman");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Mission: Guess the word correctly to save your friend!");
+
+            string[] words = new string[] { "Dog", "Fun", "Gaming", "Coding" };
+
+            var randomWordsSelector = new Random();
+            string currentWord = words[randomWordsSelector.Next(3)];
+            int livesLeft = 7;
+            Console.WriteLine("You have " + livesLeft + " lives");
+
+            for (int a = 0; a < 6; a++)
+            {
+
+            
+
+                Console.WriteLine("");
+                 for (int i = 0; i < currentWord.Length; i++)
+                 {
+                    Console.Write("-");
+                 }
+            
+            
+
+                Console.WriteLine("\n");
+                string userGuess = Console.ReadLine();
+
+                if (currentWord == userGuess)
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+                else if (currentWord != userGuess)
+                {
+                    livesLeft--;
+                    Console.WriteLine("You have " +livesLeft + " lives left \n");               
+                }
+               
+
+
+               
+
+
+
+
+            }
+
+            Console.ReadLine();
+
+        }      
+            
+    }
+    
+}
